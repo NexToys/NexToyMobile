cd8cde72b61649e7ed3788682b67f6128b072a79
yusufexak <yusufsirinn@gmail.com>
2020-11-28 14:42:42 +0300

    Notifier added

---
+++ lib/Core/Navigation/navigation_service.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class NavigatorService {
+  static NavigatorService _singleton = NavigatorService._internal();
+  factory NavigatorService() {
+    return _singleton;
+  }
+
+  NavigatorService._internal();
+
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+
+  GlobalKey<NavigatorState> get navigatorKey => _navigatorKey;
+
+  Future<bool> navigateTo(Widget route) {
+    return _navigatorKey.currentState
+        .push(MaterialPageRoute(builder: (BuildContext context) => route));
+  }
+
+  Future<bool> navigateToReplace(Widget route) {
+    return _navigatorKey.currentState.pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) => route));
+  }
+
+  void navigatePop() {
+    _navigatorKey.currentState.pop();
+  }
+}
